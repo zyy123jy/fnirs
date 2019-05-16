0e0f4e7c50535b42e0026c4d18c893e367478c7b
zyy123jy <zhuyingying2@gmail.com>
2019-05-16 18:08:32 -0400

    Update generate_data.m

--- generate_data.m
+++ generate_data.m
@@ -1,5 +1,5 @@
 load('data_original.mat')
-win_size = 400 % the sliding window size
+win_size = 400 % the sliding window size you can change this sliding window size
 % X_drug_p is the data for the subjects receiving THC X_pl_p is the data for the subject receiving placebo
 
 [F1_1] = compute_corr5(X_drug_p,win_size);
